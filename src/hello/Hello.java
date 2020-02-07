ab79fb299ae03918bf3525f795faa9c8ce38eda4
Dream4Net <Dream4Net@com009.mynet>
2020-02-07 18:29:32 +0200

    First project

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -15,6 +15,7 @@ public class Hello {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println(" HELLO WORLD");
         // TODO code application logic here
     }
     
